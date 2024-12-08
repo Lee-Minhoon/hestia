dba4207a95e79f56fb5bb5ede23343a8cfa06c5b
Lee Minhoon <hackids@naver.com>
2024-12-09 06:58:03 +0900

    feat: add view type and view type selector

--- src/components/controls/table-controls.tsx
+++ src/components/controls/table-controls.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { useForm } from "react-hook-form";
+import { CiGrid41, CiViewTable } from "react-icons/ci";
 
 import { useSearchParams } from "@/lib/hooks/use-search-params";
 import { QueryParamKeys } from "@/lib/queryParams";
 
 import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import { Input } from "../ui/input";
 import {
   Select,
@@ -22,6 +29,7 @@ const TableControls = () => {
     <div className="flex gap-4">
       <SearchForm />
       <PageSizeSelector />
+      <ViewTypeSelector />
     </div>
   );
 };
@@ -71,6 +79,54 @@ const PageSizeSelector = () => {
         ))}
       </SelectContent>
     </Select>
+  );
+};
+
+const viewTypes = [
+  {
+    label: "Table View",
+    icon: <CiViewTable className="w-4 h-4" />,
+    value: "table",
+  },
+  {
+    label: "Grid View",
+    icon: <CiGrid41 className="w-4 h-4" />,
+    value: "grid",
+  },
+];
+
+const ViewTypeSelector = () => {
+  const { searchParams, setSearchParams } = useSearchParams();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          {viewTypes.find(
+            (viewType) =>
+              viewType.value === searchParams.get(QueryParamKeys.ViewType)
+          )?.icon ?? viewTypes[0].icon}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {viewTypes.map(({ label, icon, value }) => (
+          <DropdownMenuItem
+            key={value}
+            onClick={() =>
+              setSearchParams((searchParams) => {
+                searchParams.set(QueryParamKeys.ViewType, value);
+                return searchParams;
+              })
+            }
+          >
+            <div className="flex gap-2 items-center">
+              {icon}
+              {label}
+            </div>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
