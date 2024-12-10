5362f7c86851aa31c38d21feb74bde886cca76c0
Lee Minhoon <hackids@naver.com>
2024-12-10 23:53:14 +0900

    feat: add parseCursor validation function

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,26 +1,48 @@
 import { isInteger } from "lodash-es";
 import { z } from "zod";
 
-const strToPosInt = (defaultValue: number) => {
-  return z
-    .string()
-    .optional()
-    .nullable()
+const optionalString = z.string().optional().nullable();
+
+const strToInt = (
+  defaultValue: number,
+  option?: {
+    min?: number;
+    max?: number;
+  }
+) => {
+  const min = option?.min ?? 1;
+  const max = option?.max ?? Number.MAX_SAFE_INTEGER;
+  return optionalString
     .transform((v) => {
       const num = Number(v);
-      if (isNaN(num) || !isInteger(num) || num < 1) return;
+      if (isNaN(num) || !isInteger(num) || num < min || num > max) return;
       return num;
     })
-    .pipe(z.number().int().min(1).default(defaultValue));
+    .pipe(z.number().int().min(min).max(max).default(defaultValue));
 };
 
 const paginationSchema = z.object({
-  pageIndex: strToPosInt(1),
-  pageSize: strToPosInt(10),
+  pageIndex: strToInt(1),
+  pageSize: strToInt(10),
 });
 
 export function parsePagination(
   data: Record<string, string | undefined | null>
 ) {
   return paginationSchema.parse(data);
+}
+
+const cursorSchema = z.object({
+  cursor: strToInt(0, { min: 0 }),
+  limit: strToInt(10),
+  order: z
+    .string()
+    .optional()
+    .nullable()
+    .transform((v) => (v === "desc" ? v : "asc"))
+    .default("desc"),
+});
+
+export function parseCursor(data: Record<string, string | undefined | null>) {
+  return cursorSchema.parse(data);
 }
