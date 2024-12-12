bd14eae604be2262e418e2bf7c65c0709279a08f
Lee Minhoon <hackids@naver.com>
2024-12-13 01:43:12 +0900

    feat: add UserCard component

---
+++ src/app/[locale]/(main)/users/user-card.tsx
@@ -0,0 +1,46 @@
+import { format } from "date-fns";
+import { FaUserAlt } from "react-icons/fa";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { User } from "@/lib/db/schema";
+
+interface UserCardProps {
+  data: User;
+}
+
+export default function UserCard({ data: user }: UserCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex gap-4">
+          <Avatar>
+            <AvatarImage src={user.image ?? undefined} alt="profile" />
+            <AvatarFallback>
+              <FaUserAlt />
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col overflow-hidden">
+            <CardTitle className="text-sm">{user.name}</CardTitle>
+            <CardDescription className="truncate">{user.email}</CardDescription>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        Some user details
+      </CardContent>
+      <CardFooter className="justify-center">
+        <p className="text-xs text-muted-foreground">
+          {`Joined on ${format(user.createdAt, "PPpp")}`}
+        </p>
+      </CardFooter>
+    </Card>
+  );
+}
