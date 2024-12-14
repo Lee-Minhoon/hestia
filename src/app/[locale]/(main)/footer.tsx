12477ecd048265793e33d2bfce487db39a06d0a0
Lee Minhoon <hackids@naver.com>
2024-12-15 02:42:22 +0900

    feat: add global horizontal padding to layout

--- src/app/[locale]/(main)/footer.tsx
+++ src/app/[locale]/(main)/footer.tsx
@@ -1,6 +1,6 @@
 export default function MainLayoutFooter() {
   return (
-    <div className="flex py-12 border-t-1 items-center justify-center">
+    <div className="flex px-4 py-12 border-t-1 items-center justify-center">
       Footer
     </div>
   );
--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <MainLayoutHeader />
-      <div className="flex py-4 flex-1 justify-center">
+      <div className="flex p-4 flex-1 justify-center">
         <div className="w-content">{children}</div>
       </div>
       <MainLayoutFooter />
