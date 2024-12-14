2da486de9824a34977821851db9c7b040dbebb0e
Lee Minhoon <hackids@naver.com>
2024-12-15 02:14:58 +0900

    feat: add query invalidation for user creation and deletion

--- src/app/[locale]/(main)/users/user-test-section.tsx
+++ src/app/[locale]/(main)/users/user-test-section.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+
+import { useQueryClient } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
 import { initState } from "@/lib/action";
 import { addTestUsersAction, deleteAllUsersAction } from "@/lib/actions/user";
 import useActionToast from "@/lib/hooks/use-action-toast";
+import { Endpoints, toUrl } from "@/lib/routes";
 
 export default function UserTestSection() {
   return (
@@ -17,12 +20,19 @@ export default function UserTestSection() {
 }
 
 const AddTestUsersForm = () => {
+  const queryClient = useQueryClient();
+
   const [state, dispatch, isPending] = useActionState(
     addTestUsersAction,
     initState()
   );
 
   useActionToast(state);
+
+  useEffect(() => {
+    if (state.status === "idle") return;
+    queryClient.invalidateQueries({ queryKey: [toUrl(Endpoints.Users)] });
+  }, [queryClient, state.status]);
 
   return (
     <form action={dispatch}>
@@ -34,12 +44,19 @@ const AddTestUsersForm = () => {
 };
 
 const DeleteAllUsersForm = () => {
+  const queryClient = useQueryClient();
+
   const [state, dispatch, isPending] = useActionState(
     deleteAllUsersAction,
     initState()
   );
 
   useActionToast(state);
+
+  useEffect(() => {
+    if (state.status === "idle") return;
+    queryClient.invalidateQueries({ queryKey: [toUrl(Endpoints.Users)] });
+  }, [queryClient, state.status]);
 
   return (
     <form action={dispatch}>
