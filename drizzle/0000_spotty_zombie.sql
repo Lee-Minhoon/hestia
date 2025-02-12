dd7e96b535d3dfb0edf08eca4ea68f622f60da32
Lee Minhoon <hackids@naver.com>
2025-02-12 17:42:39 +0900

    chore: regenerate database schema after changes

--- drizzle/0000_spotty_zombie.sql
+++ drizzle/0000_spotty_zombie.sql
@@ -12,6 +12,16 @@ CREATE TABLE IF NOT EXISTS "account" (
 	"session_state" text
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "post" (
+	"userId" integer NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user" (
 	"name" text NOT NULL,
 	"email" text NOT NULL,
@@ -27,6 +37,12 @@ CREATE TABLE IF NOT EXISTS "user" (
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "post" ADD CONSTRAINT "post_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
