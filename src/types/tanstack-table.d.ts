c375c9a8f5c0087453f869291676686d213bc2ef
Lee Minhoon <hackids@naver.com>
2025-04-10 00:11:51 +0900

    feat: add className support to column meta for responsive table

--- src/types/tanstack-table.d.ts
+++ src/types/tanstack-table.d.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "@tanstack/table-core";
 import { useTranslations } from "next-intl";
 
 declare module "@tanstack/table-core" {
-  // eslint-disable-next-line
   interface TableMeta<TData extends RowData> {
     searchParams: URLSearchParams;
     t: ReturnType<typeof useTranslations<never>>;
+  }
+
+  interface ColumnMeta<TData extends RowData, TValue = unknown> {
+    className?: string;
   }
 }
