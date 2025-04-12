94ff43c1738b7820f54f34d2ac2f2ba67ff6e286
Lee Minhoon <hackids@naver.com>
2025-04-12 18:02:27 +0900

    refactor: clean up query control structure

--- src/components/query-controls.tsx
+++ src/components/query-controls.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 
 import { LayoutGridIcon, SearchIcon, TableIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
@@ -16,6 +16,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Input } from "./ui/input";
 import {
   Select,
@@ -26,18 +27,9 @@ import {
 } from "./ui/select";
 
 function TableControls() {
-  const { setSearchParams } = useSearchParams();
-
   return (
     <div className="flex gap-4">
-      <SearchForm
-        onSubmit={(data) => {
-          setSearchParams((searchParams) => {
-            searchParams.set(QueryParamKeys.Search, data.search);
-            return searchParams;
-          });
-        }}
-      />
+      <SearchForm />
       <PageSizeSelector />
       <ViewTypeSelector />
     </div>
@@ -45,47 +37,122 @@ function TableControls() {
 }
 
 function GridControls() {
-  const { setSearchParams } = useSearchParams();
-
   return (
     <div className="flex gap-4">
-      <SearchForm
-        onSubmit={(data) => {
-          setSearchParams((searchParams) => {
-            searchParams.set(QueryParamKeys.Search, data.search);
-            return searchParams;
-          });
-        }}
-      />
+      <SearchForm />
       <OrderSelector />
       <ViewTypeSelector />
     </div>
   );
 }
 
-interface SearchFormProps {
-  onSubmit: (data: { [QueryParamKeys.Search]: string }) => void;
-}
-
-function SearchForm({ onSubmit }: SearchFormProps) {
+function SearchForm() {
   const t = useTranslations("Common");
 
-  const form = useForm<{ [QueryParamKeys.Search]: string }>();
+  const { setSearchParams } = useSearchParams();
+
+  const form = useForm<{ [QueryParamKeys.Search]: string }>({
+    defaultValues: {
+      [QueryParamKeys.Search]: "",
+    },
+  });
+
+  const handleSubmit = useCallback(
+    (data: { [QueryParamKeys.Search]: string }) => {
+      setSearchParams((searchParams) => {
+        searchParams.set(QueryParamKeys.Search, data.search);
+        return searchParams;
+      });
+    },
+    [setSearchParams]
+  );
 
   return (
-    <form
-      className="flex gap-2"
-      onSubmit={form.handleSubmit(useCallback(onSubmit, [onSubmit]))}
-    >
-      <Input
-        {...form.register(QueryParamKeys.Search)}
-        placeholder={t("searchPlaceholder")}
-      />
-      <Button type="submit">
-        <SearchIcon />
-        {t("search")}
-      </Button>
-    </form>
+    <Form {...form}>
+      <form className="flex gap-2" onSubmit={form.handleSubmit(handleSubmit)}>
+        <FormField
+          control={form.control}
+          name={QueryParamKeys.Search}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input {...field} placeholder={t("searchPlaceholder")} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          <SearchIcon />
+          {t("search")}
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+function ViewTypeSelector() {
+  const t = useTranslations("Common");
+
+  const { viewType, onViewTypeChange } = useViewType();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          {viewType.icon}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {viewTypes.map(({ label, icon, value }) => (
+          <DropdownMenuItem key={value} onClick={() => onViewTypeChange(value)}>
+            {icon}
+            {t(label)}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function PageSizeSelector() {
+  const t = useTranslations("Common");
+
+  const { pageSize, onPageSizeChange } = usePageSize();
+
+  return (
+    <Select value={pageSize.toString()} onValueChange={onPageSizeChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={t("rowsPerPage")} />
+      </SelectTrigger>
+      <SelectContent>
+        {rowsPerPage.map((value) => (
+          <SelectItem key={value} value={value.toString()}>
+            {value}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
+
+function OrderSelector() {
+  const t = useTranslations("Common");
+
+  const { order, onOrderChange } = useOrder();
+
+  return (
+    <Select value={order.value} onValueChange={onOrderChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={t("orderBy")} />
+      </SelectTrigger>
+      <SelectContent>
+        {orders.map(({ label, value }) => (
+          <SelectItem key={value} value={value.toString()}>
+            {t(label)}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 }
 
@@ -102,69 +169,55 @@ const viewTypes = [
   },
 ] as const;
 
-function ViewTypeSelector() {
-  const t = useTranslations("Common");
-
+function useViewType() {
   const { searchParams, setSearchParams } = useSearchParams();
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          {viewTypes.find(
-            (viewType) =>
-              viewType.value === searchParams.get(QueryParamKeys.ViewType)
-          )?.icon ?? viewTypes[0].icon}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        {viewTypes.map(({ label, icon, value }) => (
-          <DropdownMenuItem
-            key={value}
-            onClick={() => {
-              const searchParams = new URLSearchParams();
-              searchParams.set(QueryParamKeys.ViewType, value);
-              setSearchParams(searchParams);
-            }}
-          >
-            {icon}
-            {t(label)}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+  const viewType = useMemo(() => {
+    const value = searchParams.get(QueryParamKeys.ViewType);
+    return (
+      viewTypes.find((viewType) => viewType.value === value) ?? viewTypes[0]
+    );
+  }, [searchParams]);
+
+  const onViewTypeChange = useCallback(
+    (newValue: string) => {
+      if (newValue === viewType.value) {
+        return;
+      }
+
+      const searchParams = new URLSearchParams();
+      searchParams.set(QueryParamKeys.ViewType, newValue);
+      setSearchParams(searchParams);
+    },
+    [setSearchParams, viewType]
   );
+
+  return { viewType, onViewTypeChange };
 }
 
 const rowsPerPage = [10, 20, 50, 100];
 
-function PageSizeSelector() {
-  const t = useTranslations("Common");
-
+function usePageSize() {
   const { searchParams, setSearchParams } = useSearchParams();
 
-  return (
-    <Select
-      value={searchParams.get(QueryParamKeys.PageSize) ?? ""}
-      onValueChange={(value) =>
-        setSearchParams((searchParams) => {
-          searchParams.set(QueryParamKeys.PageSize, value);
-          return searchParams;
-        })
-      }
-    >
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder={t("rowsPerPage")} />
-      </SelectTrigger>
-      <SelectContent>
-        {rowsPerPage.map((value) => (
-          <SelectItem key={value} value={value.toString()}>
-            {value}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+  const pageSize = useMemo(() => {
+    const value = searchParams.get(QueryParamKeys.PageSize);
+    return (
+      rowsPerPage.find((row) => row.toString() === value) ?? rowsPerPage[0]
+    );
+  }, [searchParams]);
+
+  const onPageSizeChange = useCallback(
+    (newValue: string) => {
+      setSearchParams((searchParams) => {
+        searchParams.set(QueryParamKeys.PageSize, newValue);
+        return searchParams;
+      });
+    },
+    [setSearchParams]
   );
+
+  return { pageSize, onPageSizeChange };
 }
 
 const orders = [
@@ -178,33 +231,25 @@ const orders = [
   },
 ] as const;
 
-function OrderSelector() {
-  const t = useTranslations("Common");
-
+function useOrder() {
   const { searchParams, setSearchParams } = useSearchParams();
 
-  return (
-    <Select
-      value={searchParams.get(QueryParamKeys.Order) ?? ""}
-      onValueChange={(value) =>
-        setSearchParams((searchParams) => {
-          searchParams.set(QueryParamKeys.Order, value);
-          return searchParams;
-        })
-      }
-    >
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder={t("orderBy")} />
-      </SelectTrigger>
-      <SelectContent>
-        {orders.map(({ label, value }) => (
-          <SelectItem key={value} value={value.toString()}>
-            {t(label)}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+  const order = useMemo(() => {
+    const value = searchParams.get(QueryParamKeys.Order);
+    return orders.find((order) => order.value === value) ?? orders[0];
+  }, [searchParams]);
+
+  const onOrderChange = useCallback(
+    (newValue: string) => {
+      setSearchParams((searchParams) => {
+        searchParams.set(QueryParamKeys.Order, newValue);
+        return searchParams;
+      });
+    },
+    [setSearchParams]
   );
+
+  return { order, onOrderChange };
 }
 
 export { GridControls, TableControls };
